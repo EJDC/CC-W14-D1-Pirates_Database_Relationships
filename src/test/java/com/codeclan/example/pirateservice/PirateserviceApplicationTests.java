e8aa31e3dc32a8d73ab12a35eaf24eee64101956
EJDC <ewanconnor@gmail.com>
2023-01-30 21:42:04 +0000

    Initial commit

---
+++ src/test/java/com/codeclan/example/pirateservice/PirateserviceApplicationTests.java
@@ -0,0 +1,56 @@
+package com.codeclan.example.pirateservice;
+
+import com.codeclan.example.pirateservice.models.Pirate;
+import com.codeclan.example.pirateservice.models.Raid;
+import com.codeclan.example.pirateservice.models.Ship;
+import com.codeclan.example.pirateservice.repositories.PirateRepository;
+import com.codeclan.example.pirateservice.repositories.RaidRepository;
+import com.codeclan.example.pirateservice.repositories.ShipRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PirateserviceApplicationTests {
+
+	@Autowired
+	PirateRepository pirateRepository;
+
+
+	@Autowired
+	ShipRepository shipRepository;
+
+	@Autowired
+	RaidRepository raidRepository;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void createPirateOnShip() {
+
+		Ship ship = new Ship("The Flying Dutchman");
+		shipRepository.save(ship);
+
+		Pirate jack = new Pirate("Andy", "Sparrow", 32, ship);
+		pirateRepository.save(jack);
+	}
+
+	@Test
+	public void addPiratesAndRaids(){
+		Ship ship = new Ship("The Flying Dutchman");
+		shipRepository.save(ship);
+
+		Pirate pirate1 = new Pirate("Jack", "Sparrow", 32, ship);
+		pirateRepository.save(pirate1);
+
+		Raid raid1 = new Raid("Tortuga", 100);
+		raidRepository.save(raid1);
+
+		raid1.addPirate(pirate1);
+		raidRepository.save(raid1);
+
+	}
+
+}
